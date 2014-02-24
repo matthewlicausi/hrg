ecc42a78b62b7e42eaf9ba68088db8a40da2f53e
Matthew Licausi <mlicausi@gmail.com>
2014-02-24 12:31:26 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hrgsite::Application.config.secret_token = '87e7762e50e85901f5777d7647c2d15dba6307ef843bfe68477b90873bf121de14723fd1c55c9253b1e85fc55f88fff3f794b05a666183a06e10ced33a95cd49'
