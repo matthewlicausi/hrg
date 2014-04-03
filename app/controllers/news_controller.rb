f4c198ae610111b4e2bda850f10bb6b3822e3253
Matthew Licausi <matthewlicausi@gmail.com>
2014-04-03 06:31:46 -0500

    thrashing

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -22,6 +22,10 @@ class NewsController < ApplicationController
 	  
 	  @post_exists = true
 
+
+      @current_url = url_for :only_path=>false,:overwrite_params=>{}
+
+
 	  respond_to do |format|
 	    format.html # show.html.erb
 	    format.json { render json: @post }
