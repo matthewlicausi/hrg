0d68845eaf7267e1cf7af4594393b87181d51db6
Matthew Licausi <matthewlicausi@gmail.com>
2014-05-07 12:45:02 -0500

    modify model to correctly call the env var"

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,14 @@
 class Post < ActiveRecord::Base
   attr_accessible :author, :body, :category, :title, :user_id, :image
   
-has_attached_file :image,
+  has_attached_file :image,
     :storage => :s3,
-    :bucket => ENV['S3_BUCKET_NAME'],
+    :bucket => 'S3_BUCKET_NAME',
     :s3_credentials => {
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :access_key_id => 'AWS_ACCESS_KEY_ID',
+      :secret_access_key => 'AWS_SECRET_ACCESS_KEY'
     }
+end
   
   validates_attachment_size :image, :less_than => 10.megabytes
   do_not_validate_attachment_file_type :image
