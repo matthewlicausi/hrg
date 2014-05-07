5a4f4718613926a0b85f116f6069889b2f6af576
Matthew Licausi <matthewlicausi@gmail.com>
2014-05-07 12:49:01 -0500

    fix dumb mistake

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,13 @@
 class Post < ActiveRecord::Base
   attr_accessible :author, :body, :category, :title, :user_id, :image
   
-  has_attached_file :image,
+  has_attached_file :photo,
     :storage => :s3,
     :bucket => 'S3_BUCKET_NAME',
     :s3_credentials => {
       :access_key_id => 'AWS_ACCESS_KEY_ID',
       :secret_access_key => 'AWS_SECRET_ACCESS_KEY'
     }
-end
   
   validates_attachment_size :image, :less_than => 10.megabytes
   do_not_validate_attachment_file_type :image
