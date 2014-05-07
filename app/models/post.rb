63ca91158deaba252d3fb08fd6bb6a14a3e4b960
Matthew Licausi <matthewlicausi@gmail.com>
2014-05-07 12:57:15 -0500

    more fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,10 @@ class Post < ActiveRecord::Base
   
   has_attached_file :image,
     :storage => :s3,
-    :bucket => 'S3_BUCKET_NAME',
+    :bucket => ENV['S3_BUCKET_NAME'],
     :s3_credentials => {
-      :access_key_id => 'AWS_ACCESS_KEY_ID',
-      :secret_access_key => 'AWS_SECRET_ACCESS_KEY'
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   
   validates_attachment_size :image, :less_than => 10.megabytes
