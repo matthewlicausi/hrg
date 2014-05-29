fa20d31e3b731daebf4c423a67f0639094c7b095
Matthew Licausi <matthewlicausi@gmail.com>
2014-05-29 10:09:00 -0500

    add SyndeoHRO as an author

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,8 @@ module ApplicationHelper
 			['Beau Peters', 'Beau Peters'],
 			['Kat Wood', 'Kat Wood'],
 			['HRG Staff', 'HRG Staff'],
-			['Bill Maness', 'Bill Maness']
+			['Bill Maness', 'Bill Maness'],
+			['SyndeoHRO', 'SyndeoHRO']
 		  ]
   end
 
